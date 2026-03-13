97d5230b74d3d4902cb1bfdc278e8207016dab8e
zenbenten <joshuaelsner97@gmail.com>
2026-03-12 23:36:29 -0400

    Add set_data and randomize cell words

--- main/cell.gd
+++ main/cell.gd
@@ -1,6 +1,16 @@
 extends ColorRect
 
-var is_revealed = false
-var is_mine = false
-var number_of_neighboring_mines = 0
-const word_text = "CAT"
+@onready var word_label = %Label
+
+var grid_position: Vector2
+var target_word: String = ""
+var is_revealed: bool = false
+var is_mine: bool = false
+var number_of_neighboring_mines: int = 0
+
+func set_data(pos: Vector2, word: String):
+	grid_position = pos
+	target_word = word
+	
+	if word_label:
+		word_label.text = target_word
--- main/game_board.gd
+++ main/game_board.gd
@@ -2,8 +2,51 @@ extends GridContainer
 
 @export var cell_scene: PackedScene
 
+var word_list: Array[String] = [
+	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",
+		"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK",	"CAT", "DOG", "BAT", "ANT", "COW", "PIG", "OWL", "FOX", 
+	"BEE", "EEL", "FLY", "APE", "ELK", "BUG", "RAT", "YAK"
+]
+
 func _ready():
+	columns = 16
+	word_list.shuffle()
+	
+	#Populate game board with nodes
 	for i in range(256):
 		var new_cell = cell_scene.instantiate()
 		
+		#Assign XY coordinates
+		var x = i % columns
+		var y = i / columns
+		var grid_position = Vector2(x,y)
+		
+		var assigned_word = word_list[i]
+		
 		add_child(new_cell)
+		new_cell.set_data(grid_position, assigned_word)
+		
